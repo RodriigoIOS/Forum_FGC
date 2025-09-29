20f4b2de1a1cb4a2d8d13393601085e77fe8dd32
RodriigoiOS <Rodriigo.dev@outlook.com>
2025-09-28 23:00:35 -0300

    Criando a UITabBarController

---
+++ Forum_FGC/Forum_FGC/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Forum_FGC
+//
+//  Created by Rodrigo on 28/09/25.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
